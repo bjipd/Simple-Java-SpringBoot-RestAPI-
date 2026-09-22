d391bc97eb6afdd0dc4bb2ea67791f0acb8dc785
USER <dikeox11@gmail.com>
2026-09-22 22:35:33 +0100

    first wan

---
+++ src/main/java/com/example/RestAPI/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.RestAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String Hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/com/example/RestAPI/controllers/OrderController.java
@@ -0,0 +1,14 @@
+package com.example.RestAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    
+    @GetMapping("/order")
+    public String order(@RequestParam String item){
+        return "Your order for " + item + " has been received";
+    }
+}
